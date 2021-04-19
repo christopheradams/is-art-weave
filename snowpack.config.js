32a2826e4b4da1594abc7a5282818e988ae6a905
Christopher Adams <christopher@cxadams.com>
2021-04-19 12:00:42 +0800

    Exclude files from build

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,6 +3,12 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+  exclude: [
+    '**/contracts/**/*',
+    '**/package*.json',
+    '**/README.md',
+    '**/snowpack.config.js'
+  ],
   mount: {
     /* ... */
   },
