4c6c1e4f9368b0947671c014bad1e582d10cc11f
Christopher Adams <christopher@cxadams.com>
2021-04-19 13:18:34 +0800

    Read and display contract state

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import Arweave from 'arweave';
+import * as SmartWeave from 'smartweave';
+
+async function displayStatus(props) {
+  const state = await SmartWeave.readContract(
+    props.arweave,
+    props.contractId
+  );
+
+  console.log('[Is Art] State:', state);
+
+  const status = document.getElementById('is-art-status');
+
+  if(state.isArt) {
+    status.innerText = 'is';
+  } else {
+    status.innerText = 'is not';
+  }
+}
 
 async function main() {
   const arweave = Arweave.init();
-  arweave.network.getInfo().then(console.log);
+  arweave.network.getInfo().then((info) => {
+    console.log('[Is Art] Arweave network:', info.network);
+  });
 
-  console.log("Is Art Contract:", import.meta.env.IS_ART_CONTRACT_ID);
+  const contractId = import.meta.env.IS_ART_CONTRACT_ID;
+  console.log('[Is Art] Contract:', contractId);
+
+  displayStatus({arweave, contractId});
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
