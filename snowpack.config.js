737f6d89a14aa7cc82436a0e20f6b9eb6131e73f
Christopher Adams <christopher@cxadams.com>
2021-04-27 14:18:34 +0800

    Add build configs

---
+++ snowpack.config.js
@@ -0,0 +1,38 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+const pkg = require('./package.json')
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  env: {
+    SNOWPACK_PUBLIC_IS_ART_VERSION: pkg.version
+  },
+  exclude: [
+    '**/contracts/**/*',
+    '**/dist/**/*',
+    '**/LICENSE',
+    '**/package*.json',
+    '**/postcss.config.js',
+    '**/README.md',
+    '**/screenshot.png',
+    '**/snowpack.config.js'
+  ],
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    ['@snowpack/plugin-dotenv'],
+    ['@snowpack/plugin-postcss'],
+    ['@snowpack/plugin-webpack']
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  }
+}
